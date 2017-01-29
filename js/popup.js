38d17f69a449ff406f08969b75d2840a30056847
Danyal95 <danyal.work1@gmail.com>
2017-01-29 17:49:12 -0500

    Added comments and changed format

--- js/popup.js
+++ js/popup.js
@@ -2,11 +2,12 @@ $(document).ready(function() {
 
   $("#city-name").keypress(function(event) {
 
-    // Triggers function when "Enter" key is pressed.
+    // Only triggers function when "Enter" key is pressed
     if (event.keyCode == 13) {
 
       $("#city-name").blur();
 
+      // Open Weather API
       var api = "http://api.openweathermap.org/data/2.5/weather?q=";
       var city = $("#city-name").val();
       var apiKey = "&APPID=9beb7211705e0858e94dcd5438f8972b";
@@ -19,43 +20,44 @@ $(document).ready(function() {
 
       jQuery.ajax ({
 
+        // Fetches data from API when city name entered
         url: url,
         dataType: "json",
         success: function(response) {
           $("#temp").html(Math.round(response.main.temp) + "&#186 C");
-          $("#temp-min").html("&#x2193" + response.main.temp_min + "&#186");
-          $("#temp-max").html("&#x2191" + response.main.temp_max + "&#186");
+          $("#temp-min").html("&#x2193" + Math.round(response.main.temp_min) + "&#186");
+          $("#temp-max").html("&#x2191" + Math.round(response.main.temp_max) + "&#186");
           $("#weather-main").html(response.weather[0].main);
           // $("#weather-icon").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
 
-          // Change background image to match the current weather.
+          // Change background image to match the current weather
           if (response.weather[0].description == "clear sky") {
-            $('body').css('backgroundImage', 'url(images/clear-sky.jpg)');
+            $('body').css('backgroundImage', 'url(img/clear-sky.jpg)');
             $("body").css("color", "white");
             $("#city-name").css("color", "white");
           }
           else if ((response.weather[0].description == "few clouds") || (response.weather[0].description == "scattered clouds") || (response.weather[0].description == "broken clouds") || (response.weather[0].description == "overcast clouds")) {
-            $('body').css('backgroundImage', 'url(images/clouds.jpg)');
+            $('body').css('backgroundImage', 'url(img/clouds.jpg)');
             $("body").css("color", "white");
             $("#city-name").css("color", "white");
           }
           else if ((response.weather[0].description == "shower rain") || (response.weather[0].description == "rain")) {
-            $('body').css('backgroundImage', 'url(images/rain.jpg)');
+            $('body').css('backgroundImage', 'url(img/rain.jpg)');
             $("body").css("color", "white");
             $("#city-name").css("color", "white");
           }
           else if (response.weather[0].description == "thunderstorm") {
-            $('body').css('backgroundImage', 'url(images/thunderstorm.png)');
+            $('body').css('backgroundImage', 'url(img/thunderstorm.png)');
             $("body").css("color", "white");
             $("#city-name").css("color", "white");
           }
           else if (response.weather[0].description == "snow") {
-            $('body').css('backgroundImage', 'url(images/snow.jpg)');
+            $('body').css('backgroundImage', 'url(img/snow.jpg)');
             $("body").css("color", "white");
             $("#city-name").css("color", "white");
           }
           else if (response.weather[0].description === "mist") {
-            $('body').css('backgroundImage', 'url(images/mist.jpg)');
+            $('body').css('backgroundImage', 'url(img/mist.jpg)');
             $("body").css("color", "white");
             $("#city-name").css("color", "white");
           }
@@ -68,11 +70,12 @@ $(document).ready(function() {
 
   });
 
-  function refresh () {
+  // Refreshes weather after certain time (currently not working)
+  /* function refresh () {
     setTimeout(function() {
       $("weather-info").fadeOut().load("popup.js").fadeIn();
       refresh();
     }, 500);
-  }
+  } */
 
 });
